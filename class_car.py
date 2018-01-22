4de6c38cc2da1036935e12fb88c1c011dc2fc635
Anna Tsolakou <tsolakou.a@gmail.com>
2018-01-22 22:08:52 +0100

    Update class_car.py

--- class_car.py
+++ class_car.py
@@ -3,7 +3,7 @@ class Car(object):
   def __init__(self, model, color, mpg):
     self.model = model
     self.color = color
-    self.mpg   = mpg
+    self.mpg = mpg
    
   def display_car(self):
     print "This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg))
